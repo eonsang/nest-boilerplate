85196f78c83f92d5d6229a43849b3f1f423e6527
datamond <datamond@spector-MacBook-Pro.local>
2025-04-29 16:40:26 +0900

    feat: 회원가입 로직 개발 중

--- src/auth/application/service/signup.service.test.ts
+++ src/auth/application/service/signup.service.test.ts
@@ -1,14 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SignupService } from './signup.service';
-import { USER_REPOSITORY } from 'src/auth/domain/repository';
-import { UserPrismaRepository } from 'src/auth/infra/repository/user.repository';
+import { AUTH_REPOSITORY, USER_REPOSITORY } from 'src/auth/domain/repository';
+import { EMAIL_SENDER } from '../port/out';
+import { GenerateJwtService } from './generateJwt.service';
 
 describe('SignupService', () => {
   let service: SignupService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SignupService, { provide: USER_REPOSITORY, useValue: {} }],
+      providers: [
+        SignupService,
+        GenerateJwtService,
+        { provide: USER_REPOSITORY, useValue: {} },
+        { provide: EMAIL_SENDER, useValue: {} },
+        { provide: AUTH_REPOSITORY, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<SignupService>(SignupService);
