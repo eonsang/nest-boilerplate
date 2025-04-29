62907108b94777d89c9bc89d6046db7b62785917
datamond <datamond@spector-MacBook-Pro.local>
2025-04-29 15:44:04 +0900

    test: 회원가입 테스트 케이스 추가

--- src/auth/application/service/signup.service.test.ts
+++ src/auth/application/service/signup.service.test.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SignupService } from './signup.service';
+import { USER_REPOSITORY } from 'src/auth/domain/repository';
+import { UserPrismaRepository } from 'src/auth/infra/repository/user.repository';
 
 describe('SignupService', () => {
   let service: SignupService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SignupService],
+      providers: [SignupService, { provide: USER_REPOSITORY, useValue: {} }],
     }).compile();
 
     service = module.get<SignupService>(SignupService);
